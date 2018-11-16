1b6f5341e705880ed8b993313492ab3165ab07b3
Team2.Siewmun <2355355L@student.gla.ac.uk>
2018-11-16 13:32:28 +0800

    setting post request

--- techtrek/index.js
+++ techtrek/index.js
@@ -1,6 +1,6 @@
 
 
-$(document).ready(()=>{
+$(document).ready(() => {
 
 	var query = window.location.search.substring(1);
 	var qs = parse_query_string(query);
@@ -23,20 +23,17 @@ $(document).ready(()=>{
 
 	$('#showdetails').hide();
 	$('#loadingtext').hide();
-	
+
 	//if no query params, i.e its the homepage route
-	if(qs[empty])
-	{
+	if (qs[empty]) {
 		$('#loginlink').show();
 		$('#showdetails').hide();
 	}
-	else
-	{
+	else {
 		// if the code parameter is there, 
 		// then we have been redirected by the DBS OAuth login page
-		if(qs['code'])
-		{
-			var code = qs["code"]	;
+		if (qs['code']) {
+			var code = qs["code"];
 			var state = qs["state"];
 
 			GetOAuthToken();
@@ -46,67 +43,65 @@ $(document).ready(()=>{
 		// if the get parameter is there,
 		// then we have successfully retrieve the oauth token
 		// and we can proceed to retrieve and display data from the other APIs
-		else if(qs['get'])
-		{
+		else if (qs['get']) {
 			$('#login').hide();
 			$('#loadingtext').hide();
 			$('#showdetails').show();
 
 		}
-		
+
 	}
 
 	//function to parse query parameters in the url
 	function parse_query_string(query) {
-	  var vars = query.split("&");
-	  var query_string = {};
-	  for (var i = 0; i < vars.length; i++) {
-	    var pair = vars[i].split("=");
-	    var key = decodeURIComponent(pair[0]);
-	    var value = decodeURIComponent(pair[1]);
-	    // If first entry with this name
-	    if (typeof query_string[key] === "undefined") {
-	      query_string[key] = decodeURIComponent(value);
-	      // If second entry with this name
-	    } else if (typeof query_string[key] === "string") {
-	      var arr = [query_string[key], decodeURIComponent(value)];
-	      query_string[key] = arr;
-	      // If third or later entry with this name
-	    } else {
-	      query_string[key].push(decodeURIComponent(value));
-	    }
-	  }
-	  return query_string;
+		var vars = query.split("&");
+		var query_string = {};
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
+			var key = decodeURIComponent(pair[0]);
+			var value = decodeURIComponent(pair[1]);
+			// If first entry with this name
+			if (typeof query_string[key] === "undefined") {
+				query_string[key] = decodeURIComponent(value);
+				// If second entry with this name
+			} else if (typeof query_string[key] === "string") {
+				var arr = [query_string[key], decodeURIComponent(value)];
+				query_string[key] = arr;
+				// If third or later entry with this name
+			} else {
+				query_string[key].push(decodeURIComponent(value));
+			}
+		}
+		return query_string;
 	}
 
 	// This function gets the Oauth2 token based on the
 	// authorization code that was given in the url params
 	// The authorization code is also given by OAuth2 after
 	// the user is logged in and redirected back.
-	function GetOAuthToken()
-	{
+	function GetOAuthToken() {
 		var settings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": corsbypass + "https://www.dbs.com/sandbox/api/sg/v1/oauth/tokens",
-		  "method": "POST",
-		  "headers": {
-		    "authorization": "Basic M2EwOGZkMmYtYmJhNy00YzNhLWI3M2UtNTE1NjYyZWVlMzUwOjMyNjA4MTA1LWY0MGQtNDg1Ny04ZTk5LWNlYjA4MWExZjc1YQ==",
-		    "content-type": "application/x-www-form-urlencoded",
-		    "cache-control": "no-cache"
-		  },
-		  "data": {
-		    "code": qs["code"],
-		    "grant_type": "token",
-		    "redirect_url": "http://localhost:8080/Hackaton/index.html?get=morgage"
-		  }
+			"async": true,
+			"crossDomain": true,
+			"url": corsbypass + "https://www.dbs.com/sandbox/api/sg/v1/oauth/tokens",
+			"method": "POST",
+			"headers": {
+				"authorization": "Basic M2EwOGZkMmYtYmJhNy00YzNhLWI3M2UtNTE1NjYyZWVlMzUwOjMyNjA4MTA1LWY0MGQtNDg1Ny04ZTk5LWNlYjA4MWExZjc1YQ==",
+				"content-type": "application/x-www-form-urlencoded",
+				"cache-control": "no-cache"
+			},
+			"data": {
+				"code": qs["code"],
+				"grant_type": "token",
+				"redirect_url": "http://localhost:8080/Hackaton/index.html?get=morgage"
+			}
 		}
 
 		$.ajax(settings).done(function (response) {
 			console.log(response);
 			api_access = response;
 
-	  		$('#loginlink').hide();
+			$('#loginlink').hide();
 			$('#login').hide();
 			GetApplicantData();
 
@@ -116,45 +111,59 @@ $(document).ready(()=>{
 	// this function gets the list of 
 	// morgage loan applicants and display
 	// them in a table
-	function GetApplicantData()
-	{
+	function GetApplicantData() {
 		applicantData = {
 			'Client Id': clientId,
 			'Client secret': clientSecret,
 		}
-	  	var applicantSettings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": corsbypass + "https://www.dbs.com/sandbox/api/sg/v1/mortgages/packages?partyId=12867303374682056150002&loanType=PROD_HL&propertyType=Landed%20(Private)",
-		  "method": "GET",
-		  "headers": {
-		    "content-type": "application/json",
-		    "accesstoken": api_access["access_token"],
-		    "clientid": clientId,
-		    "authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiIDogImh0dHBzOi8vY2FwaS5kYnMuY29tIiwiaWF0IiA6IDE1NDIzNDIyMTkwMzQsICJleHAiIDogMTU0MjM0NTgxOTAzNCwic3ViIiA6ICJTVmN3TXpZPSIsInB0eXR5cGUiIDogMSwiY2xuaWQiIDogIjNhMDhmZDJmLWJiYTctNGMzYS1iNzNlLTUxNTY2MmVlZTM1MCIsImNsbnR5cGUiIDogIjIiLCAiYWNjZXNzIiA6ICIxRkEiLCJzY29wZSIgOiAiMkZBLVNNUyIgLCJhdWQiIDogImh0dHBzOi8vY2FwaS5kYnMuY29tL2FjY2VzcyIgLCJqdGkiIDogIjgyMDU0NDMyMTI3NDU3OTMyNDYiICwiY2luIiA6ICJRMGxPTURBd01EQXgifQ.FK7DveyAzQSMPFa42XT_elJjs28A5TxYKGJ00AGNGsc",
-		    "cache-control": "no-cache"
-		  },
-		  "data": JSON.stringify(applicantData)
+		var applicantSettings = {
+			"async": true,
+			"crossDomain": true,
+			"url": corsbypass + "https://www.dbs.com/sandbox/api/sg/v1/mortgages/170711048/applicants",
+			"method": "GET",
+			"headers": {
+				"content-type": "application/json",
+				"accesstoken": api_access["access_token"],
+				"clientid": clientId,
+				"authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiIDogImh0dHBzOi8vY2FwaS5kYnMuY29tIiwiaWF0IiA6IDE1NDIzNDIyMTkwMzQsICJleHAiIDogMTU0MjM0NTgxOTAzNCwic3ViIiA6ICJTVmN3TXpZPSIsInB0eXR5cGUiIDogMSwiY2xuaWQiIDogIjNhMDhmZDJmLWJiYTctNGMzYS1iNzNlLTUxNTY2MmVlZTM1MCIsImNsbnR5cGUiIDogIjIiLCAiYWNjZXNzIiA6ICIxRkEiLCJzY29wZSIgOiAiMkZBLVNNUyIgLCJhdWQiIDogImh0dHBzOi8vY2FwaS5kYnMuY29tL2FjY2VzcyIgLCJqdGkiIDogIjgyMDU0NDMyMTI3NDU3OTMyNDYiICwiY2luIiA6ICJRMGxPTURBd01EQXgifQ.FK7DveyAzQSMPFa42XT_elJjs28A5TxYKGJ00AGNGsc",
+				"cache-control": "no-cache"
+			},
+			"data": JSON.stringify(applicantData)
 		}
 
 		$.ajax(applicantSettings).done(function (response) {
-		  
-		  $('#showdetails').show();
 
-		  //render each row based on the data we retrieved
-		  for(var i =0; i < response.applicantList.length; i++)
-		  {
-		  	var elem = response.applicantList[i];
-		  	var tabletext = `
+			$('#showdetails').show();
+
+			//render each row based on the data we retrieved
+			for (var i = 0; i < response.applicantList.length; i++) {
+				var elem = response.applicantList[i];
+				var tabletext = `
 		  	<tr class="tablerow">
 		  		<td>` + elem.applicant.applicantId + `</td>
 		  		<td>` + elem.applicant.name.salutation + `</td>
 		  		<td>` + elem.applicant.name.fullName + `</td>
 			 </tr>
 		  					`
-		  	$('#tablebod').append(tabletext);
-		  }
-		  
+				$('#tablebod').append(tabletext);
+			}
+
+		});
+	}
+
+	function GetAnalytics() {
+		$.ajax({
+			url: '/submit_net',
+			dataType: 'json',
+			data: JSON.stringify({}),
+			type: 'POST',
+			success: function (response) {
+				console.log(response);
+			},
+			error: function (error) {
+				console.log(error);
+			}
+
 		});
 	}
 });
