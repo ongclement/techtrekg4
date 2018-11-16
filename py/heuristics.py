c6e2335cbeb704cf629af56c9dc7d4ebb0829177
Alvin <ikoori@live.com>
2018-11-16 13:12:45 +0800

    For nic

--- py/heuristics.py
+++ py/heuristics.py
@@ -1,11 +1,30 @@
-class Data:
-  age         = None
-  salary      = None
-  familySize = None
-  childrenAge = None
+class InputData:
+  salary        = None
+  familySize    = None
+  fcontribution = None
+  presentSavings = None
+  investmentGoal = None
+
+class OutputData:
+  plan = None
+  investment = None
+  emergency  = None
+  disposable = None
+  repayment  = None
 
   def __init__(self):
     return
 
 def ParseData(data):
   # grab all data
+  # optimal plan
+  # year
+  # repaymentamount per month
+  # savings
+
+
+  return
+
+def SortData(data):
+  return
+
